a227f76e708ecfd95f0e00c8a2a595c1cc56b892
bananaback <58079308+bananaback@users.noreply.github.com>
2024-10-04 18:39:21 +0700

    feat(login): install tailwindcss and create UI for login page

---
+++ LetWeCook.Web/wwwroot/js/login.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    // Call the function on document ready
+    handleSlideNavBtnClick();
+});
+
+// Function to handle slide-nav-btn click event
+function handleSlideNavBtnClick() {
+    $('.slide-nav-btn').on('click', function () {
+        // When a button is clicked, change its background color
+        $(this).siblings().removeClass('bg-gray-600').addClass('bg-slate-50'); // Remove active state from siblings
+
+        $(this).removeClass('bg-slate-50').addClass('bg-gray-600');
+    });
+}
