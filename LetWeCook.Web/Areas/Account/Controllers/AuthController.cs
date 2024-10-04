70c9098fe0eb694a6a2bdef7182b42389a9b8946
bananaback <58079308+bananaback@users.noreply.github.com>
2024-10-04 18:39:21 +0700

    feat(login): install tailwindcss and create UI for login page

---
+++ LetWeCook.Web/Areas/Account/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LetWeCook.Web.Areas.Account.Controllers
+{
+    [Area("Account")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ LetWeCook.Web/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    './Areas/**/*.cshtml',
+    './Views/**/*.cshtml',
+    './wwwroot/**/*.html',
+    './wwwroot/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
